5c96527ed2c41ab4a6b6664eb92a0e9a7e642fc7
Radityairawan2410432003 <irawanraditya2006@gmail.com>
2025-06-11 09:56:16 +0700

    Add files via upload

---
+++ tugas 9 raditya irawan soal 1.py
@@ -0,0 +1,18 @@
+from termcolor import cprint, colored
+
+cprint(" " * 31 + colored(" " *19, 'red', 'on_red'))
+cprint(" " * 33 + colored(" " *19, 'red', 'on_red'))
+cprint(" " * 35 + colored(" " *19, 'red', 'on_red'))
+cprint(" " * 37 + colored(" " *19, 'red', 'on_red'))
+cprint(" " * 39 + colored(" " *19, 'red', 'on_red'))
+print("")
+cprint(" " * 17 + colored(" " *17, 'red', 'on_blue') + " " * 5 + colored(" "*19, 'green', 'on_green')) 
+cprint(" " * 15 + colored(" " *17, 'red', 'on_blue') + " " * 5 + colored(" "*19, 'green', 'on_green')) 
+cprint(" " * 13 + colored(" " *17, 'red', 'on_blue') + " " * 5 + colored(" "*19, 'green', 'on_green')) 
+cprint(" " * 11 + colored(" " *17, 'red', 'on_blue') + " " * 5 + colored(" "*19, 'green', 'on_green')) 
+cprint(" " * 9 + colored(" " *17, 'red', 'on_blue') + " " * 5 + colored(" "*19, 'green', 'on_green')) 
+cprint(" " * 7 + colored(" " *17, 'red', 'on_blue'))
+cprint(" " * 5 + colored(" " *17, 'red', 'on_blue'))
+cprint(" " * 3 + colored(" " *17, 'red', 'on_blue'))
+
+print(colored("PERTAMINA", "cyan", attrs=["bold"]))
