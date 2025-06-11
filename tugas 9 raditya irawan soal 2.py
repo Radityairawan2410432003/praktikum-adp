17cda8df393e5baaa44c3e79350d8f6d3aea21d9
Radityairawan2410432003 <irawanraditya2006@gmail.com>
2025-06-11 09:57:22 +0700

    Add files via upload

---
+++ tugas 9 raditya irawan soal 2.py
@@ -0,0 +1,16 @@
+from termcolor import cprint, colored
+
+cprint(" " * 52 + colored(" " *7, 'white', 'on_white'))
+cprint(" " * 48 + colored(" " *13, 'white', 'on_white'))
+cprint(" " * 46 + colored(" " *17, 'white', 'on_white'))
+cprint(" " * 46 + colored(" " *18, 'white', 'on_white'))
+cprint(" " * 38 + colored(" " *4, 'white', 'on_white') + " " * 6 + colored(" "*18, 'white', 'on_white')) 
+cprint(" " * 34 + colored(" " *9, 'white', 'on_white') + " " * 6 + colored(" "*18, 'white', 'on_white')) 
+cprint(" " * 30 + colored(" " *15, 'white', 'on_white') + " " * 6 + colored(" "*17, 'white', 'on_white')) 
+cprint(" " * 28 + colored(" " *18, 'white', 'on_white') + " " * 6 + colored(" "*18, 'white', 'on_white')) 
+cprint(" " * 29 + colored(" " *18, 'white', 'on_white') + " " * 6 + colored(" "*17, 'white', 'on_white')) 
+cprint(" " * 20 + colored(" " *4, 'white', 'on_white') + " " * 6 + colored(" "*18, 'white', 'on_white')  + " " * 6 + colored(" "*18, 'white', 'on_white')) 
+cprint(" " * 18 + colored(" " *8, 'white', 'on_white') + " " * 6 + colored(" "*17, 'white', 'on_white')  + " " * 6 + colored(" "*18, 'white', 'on_white')) 
+cprint(" " * 16 + colored(" " *12, 'white', 'on_white') + " " * 6 + colored(" "*16, 'white', 'on_white')  + " " * 6 + colored(" "*18, 'white', 'on_white')) 
+
+print(colored("ADIDAS", "white", attrs=["bold"]))
